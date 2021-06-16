c5833fd0964ab4c7de0d3238b17d8c944a03a4dc
Crazy Genius <elyudde@gmail.com>
2021-06-16 16:05:15 +0300

    fixed http certificate issue

--- lib/src/utils/pesalink_prosses_transaction.dart
+++ lib/src/utils/pesalink_prosses_transaction.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:http/http.dart' as http;
 import 'package:coopbank_pesalink/coopbank_pesalink.dart';
+import 'package:http/io_client.dart';
 
 const String testUrl = 'https://openapi-sandbox.co-opbank.co.ke';
 const String prodUrl = 'https://openapi-sit.co-opbank.co.ke';
@@ -17,7 +19,14 @@ Future<BankReponse> pesalinkProcessTransaction(
   }
 
   try{
-    final http.Response _res = await http.post(Uri.parse(finalUrl), headers: headers, body: json.encode(payload));
+
+    const bool trustSelfSigned = true;
+    final HttpClient httpClient = HttpClient()
+      ..badCertificateCallback =
+          ((X509Certificate cert, String host, int port) => trustSelfSigned);
+    final IOClient ioClient = IOClient(httpClient);
+    
+    final http.Response _res = await ioClient.post(Uri.parse(finalUrl), headers: headers, body: json.encode(payload));
     dynamic _body;
     Map<String, dynamic>? _bodyAsmap;
     try{
